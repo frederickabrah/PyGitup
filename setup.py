8eafe7d23ce82bfcc1833f6425a92d63f4ff61d1
frederickabrah <frederickabraham547@gmail.com>
2026-02-04 00:40:01 +0100

    Release: v2.0.0 - The Total Immersion Milestone (Major TUI Overhaul)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygitup',
-    version='1.9.0',
+    version='2.0.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
